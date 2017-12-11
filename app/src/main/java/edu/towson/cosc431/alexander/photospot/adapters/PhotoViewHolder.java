25d129bd7dbf3a23e462e42c07184b97bc9df0ab
obviousrebel <jalexa14@students.towson.edu>
2017-12-11 18:52:47 -0500

    Image path fixes

--- app/src/main/java/edu/towson/cosc431/alexander/photospot/adapters/PhotoViewHolder.java
+++ app/src/main/java/edu/towson/cosc431/alexander/photospot/adapters/PhotoViewHolder.java
@@ -1,11 +1,14 @@
 package edu.towson.cosc431.alexander.photospot.adapters;
 
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
+
+import java.io.File;
 
 import edu.towson.cosc431.alexander.photospot.interfaces.IController;
 import edu.towson.cosc431.alexander.photospot.R;
@@ -43,6 +46,13 @@ public class PhotoViewHolder extends RecyclerView.ViewHolder {
     public void bindTodo(Photo photo) {
         this.photo = photo;
         photoTitle.setText(photo.getTitle());
-        Picasso.with(itemView.getContext()).load(photo.getImageURL()).fit().into(imageView);
+
+        if(!photo.getImageURL().contains("http")) {
+            Uri uri = Uri.fromFile(new File(photo.getImageURL()));
+            Picasso.with(itemView.getContext()).load(uri).fit().into(imageView);
+        } else {
+            Picasso.with(itemView.getContext()).load(photo.getImageURL()).fit().into(imageView);
+        }
+
     }
 }
